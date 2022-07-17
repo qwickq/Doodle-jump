9ecd02c746b459ebf5d2a31d15cf584a88f46aa0
qwickq <96002006+qwickq@users.noreply.github.com>
2022-07-17 11:31:40 +0300

    Добавление фона и главного героя
    Добавили фон, добавили героя(герой пока что не виден из-за размера окна), упрощён запуск игры.

--- game/main.py
+++ game/main.py
@@ -1,16 +1,36 @@
-from arcade import Window
-from arcade import run
+from arcade import Window, Sprite, SpriteList
+from arcade import run, set_background_color
+from arcade import color
 
 
 class MyWindow(Window):
     """Главное окно в игре"""
-    def __init__(self):
+    def __init__(self, background: tuple[int, int, int] = color.PURPLE_NAVY):
         """Конструктор для создания обычного окна"""
         super().__init__(fullscreen=False)
+
+        self.hero: Sprite = ...
+        self.players: SpriteList = ...
+
+        set_background_color(background)
+
+    def setup(self):
+        """Загружает и создает все необходимые объекты для игры/уровня/режима"""
+        self.hero = Sprite("assets/main_hero.png", 1.5)
+        
+
+        self.players = SpriteList()
+        self.players.append(self.hero)
+
+    def on_draw(self):
+        """Прорисовка всех объектов и структур на экране"""
+        self.clear()
+        self.players.draw()
 
 
 def game(window: MyWindow = MyWindow()):
     """Запускает игру"""
+    window.setup()
     run()
 
 

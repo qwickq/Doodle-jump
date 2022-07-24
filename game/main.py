ba3ccead696a9a3a80ee18f339f097333d689be0
qwickq <96002006+qwickq@users.noreply.github.com>
2022-07-24 12:35:31 +0300

    картинки, платформы, герой, задний фон
    Создана папка для картинок
    
    Добавили фон, добавили героя(герой пока что не виден из-за размера окна), упрощён запуск игры.
    
    Теперь все картинки 320 x 320

--- game/main.py
+++ game/main.py
@@ -1,16 +1,36 @@
-from arcade import Window
-from arcade import run
+from arcade import Sprite, SpriteList, Window, color, run, set_background_color
 
 
 class MyWindow(Window):
     """Главное окно в игре"""
-    def __init__(self):
+
+    def __init__(self, background: tuple[int, int, int] = color.PURPLE_NAVY):
         """Конструктор для создания обычного окна"""
         super().__init__(fullscreen=False)
+
+        self.hero: Sprite = ...
+        self.players: SpriteList = ...
+
+        set_background_color(background)
+
+    def setup(self):
+        """Загружает и создает все необходимые объекты для игры/уровня/режима"""
+        self.hero = Sprite("assets/main_hero.png", 1)
+        self.hero.center_x = 100
+        self.hero.center_y = 100
+
+        self.players = SpriteList()
+        self.players.append(self.hero)
+
+    def on_draw(self):
+        """Прорисовка всех объектов и структур на экране"""
+        self.clear()
+        self.players.draw()
 
 
 def game(window: MyWindow = MyWindow()):
     """Запускает игру"""
+    window.setup()
     run()
 
 
